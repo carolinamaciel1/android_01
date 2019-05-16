4c12681f1853bcf6d17d9f19c256a251c702de37
carolinamaciel2 <carolinamaciel37@gmail.com>
2019-05-16 01:02:56 -0300

    Criando o menu de categorias "Exatas, Humanas, Bio,Ling"

--- Pj12/app/src/main/java/com/example/pj1/ControleActivity.java
+++ Pj12/app/src/main/java/com/example/pj1/ControleActivity.java
@@ -2,8 +2,14 @@ package com.example.pj1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class ControleActivity extends Activity {
+
+    private TextView introducao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
